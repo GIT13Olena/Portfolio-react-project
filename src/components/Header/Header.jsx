03b8155f75a4c81e5269717dde6ffde0e8752048
GIT13Olena <girnyak.1olena@gmail.com>
2024-05-02 15:37:53 +0300

    replacement of resume with an updated one and change of position

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import download from "../../img/icons/download CV.svg"
-import resume from "../../img/ResumePdf/Resume Hirniak_Olena_JuniorFullStackDeveloper.pdf"
+import resume from "../../img/ResumePdf/Resume Hirniak_Olena_Frontend_Developer.pdf"
 
 function Header () {
    return (
@@ -7,12 +7,12 @@ function Header () {
           <div className="header__wrapper">
               <h1 className="header__title">
                   <strong>Hello, my name is <em>Olena</em></strong><br/>
-                  I'm a Full Stack developer
+                  I'm a Frontend developer
               </h1>
               <div className="header__text">
                   <p>My projects are creative magic that creates the future.</p>
               </div>
-              <a href={resume} download="Resume Hirniak_Olena_JuniorFullStackDeveloper" className="btn">
+              <a href={resume} download="Resume Hirniak_Olena_Frontend_Developer" className="btn">
                   <img src={download} alt="Link" />
                   Download CV 
               </a>
